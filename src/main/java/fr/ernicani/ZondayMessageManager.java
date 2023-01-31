2bf53fed42636c0eef598a1b07669a854cc40431
Thomas Lindeker <45493414+ernivani@users.noreply.github.com>
2023-01-31 21:16:07 +0100

    supprime toute les erreurs

--- src/main/java/fr/ernicani/ZondayMessageManager.java
+++ src/main/java/fr/ernicani/ZondayMessageManager.java
@@ -49,54 +49,61 @@ public final class ZondayMessageManager extends Plugin {
             return;
         }
         Logger.getLogger("Minecraft").info("§b[WebSocket] Serveur de socket créé !");
-        listenThread = new Thread(() -> {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                Logger.getLogger("Minecraft").info("§c[WebSocket] Erreur survenue lors de la mise en veille du thread d'écoute : " + e);
-                Thread.currentThread().interrupt();
-            }
-            while (!Thread.interrupted()) {
+
+        try {
+            listenThread = new Thread(() -> {
+                if (Thread.interrupted()) {
+                    return;
+                }
                 try {
-                    Socket socket = serverSocket.accept();
-                    BufferedReader socketReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                    String request = socketReader.readLine();
-                    String[] parts = request.split("\\?");
-                    String[] params = parts[1].split("&");
-                    String server = "";
-                    String command = "";
-                    String player = "";
-
-                    for (String param : params) {
-                        if (param.startsWith("server=")) {
-                            server = param.substring(7).replaceAll("%3A", ":");
-                        } else if (param.startsWith("command=")) {
-                            command = param.substring(8).replaceAll("%20", " ");
-                        }
-                        else if (param.startsWith("player=")) {
-                            player = param.substring(7).replaceAll("%20", " ");
-                        }
-                    }
-                    Logger.getLogger("Minecraft").info("§b[WebSocket] "+ server + " : " + command + " pour le joueur " + player);
-
-                    String data = "command=" + command + "&player=" + player;
-
-                    boolean sended = sendCustomDataToServer(server, data);
-                    if (sended) {
-                        Logger.getLogger("Minecraft").info("§b[WebSocket] Commande envoyée au serveur " + server + " : " + command);
-                    } else {
-                        Logger.getLogger("Minecraft").info("§c[WebSocket] Erreur survenue lors de l'envoi de la commande au serveur " + server + " : " + command);
-                    }
-
-                    socket.close();
-
-                } catch (IOException e) {
-                    e.printStackTrace();
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    Logger.getLogger("Minecraft").info("§c[WebSocket] Erreur survenue lors de la mise en veille du thread d'écoute : " + e);
                     Thread.currentThread().interrupt();
                 }
-            }
-        });
-        listenThread.start();
+                try {
+                while (!Thread.interrupted()) {
+                        Socket socket = serverSocket.accept();
+                        BufferedReader socketReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        String request = socketReader.readLine();
+                        String[] parts = request.split("\\?");
+                        String[] params = parts[1].split("&");
+                        String server = "";
+                        String command = "";
+                        String player = "";
+
+                        for (String param : params) {
+                            if (param.startsWith("server=")) {
+                                server = param.substring(7).replaceAll("%3A", ":").replaceAll("%2F", "/");
+                            } else if (param.startsWith("command=")) {
+                                command = param.substring(8).replaceAll("%20", " ").replaceAll("%2F", "/").replaceAll("%3A", ":").replaceAll("%3D", "=");
+                            } else if (param.startsWith("player=")) {
+                                player = param.substring(7).replaceAll("%20", " ");
+                            }
+                        }
+                        Logger.getLogger("Minecraft").info("§b[WebSocket] " + server + " : " + command + " pour le joueur " + player);
+
+                        String data = "command=" + command + "&player=" + player;
+
+                        boolean sended = sendCustomDataToServer(server, data);
+                        if (sended) {
+                            Logger.getLogger("Minecraft").info("§b[WebSocket] Commande envoyée au serveur " + server + " : " + command);
+                        } else {
+                            Logger.getLogger("Minecraft").info("§c[WebSocket] Erreur survenue lors de l'envoi de la commande au serveur " + server + " : " + command);
+                        }
+
+                        socket.close();
+
+                    }
+                } catch (IOException e) {
+                    Logger.getLogger("Minecraft").info("§c[WebSocket] Erreur survenue lors de l'écoute du serveur de socket : " + e);
+                }
+            });
+            listenThread.start();
+        }
+        catch (Exception e) {
+            Logger.getLogger("Minecraft").info("§c[WebSocket] Erreur survenue lors de la création du thread d'écoute : " + e);
+        }
     }
 
     public boolean sendCustomDataToServer(String serverTarget, String data) {
